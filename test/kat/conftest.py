8f811ed450885862d5200c375874652a10ed4438
Łukasz Piątkowski <piontec@gmail.com>
2020-05-18 13:15:28 +0200

    test works on kind

--- test/kat/conftest.py
+++ test/kat/conftest.py
@@ -3,12 +3,30 @@ import pykube
 
 
 def pytest_addoption(parser):
-    parser.addoption("--kubeconfig", action="store")
+    parser.addoption("--kube-config", action="store")
+    parser.addoption("--values-file", action="store")
+    parser.addoption("--chart-name", action="store")
+    parser.addoption("--chart-version", action="store")
 
 
 @pytest.fixture(scope="module")
-def kubeconfig(pytestconfig):
-    return pytestconfig.getoption("kubeconfig")
+def chart_name(pytestconfig) -> str:
+    return pytestconfig.getoption("chart_name")
+
+
+@pytest.fixture(scope="module")
+def chart_version(pytestconfig) -> str:
+    return pytestconfig.getoption("chart_version")
+
+
+@pytest.fixture(scope="module")
+def values_file_path(pytestconfig) -> str:
+    return pytestconfig.getoption("values_file")
+
+
+@pytest.fixture(scope="module")
+def kubeconfig(pytestconfig) -> str:
+    return pytestconfig.getoption("kube_config")
 
 
 @pytest.fixture(scope="module")
