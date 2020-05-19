9aa27cc248eb02d6e1ea7f3242d6721bf01b982e
Łukasz Piątkowski <piontec@gmail.com>
2020-05-19 16:57:54 +0200

    WIP: adding node affitnity

--- test/kat/test_nginx_ic.py
+++ test/kat/test_nginx_ic.py
@@ -1,5 +1,5 @@
 from pykube import Job, Pod
-from typing import Iterator, Callable
+from typing import Iterator, Callable, List, Tuple
 from conftest import GatlingAppFactoryFunc, StormforgerLoadAppFactoryFunc
 from parsers.gatling_parser import GatlingParser
 import pykube.objects
@@ -17,9 +17,30 @@ def wait_for_job(job: Job):
         time.sleep(1)
 
 
+def get_affinity_nodes(kube_client: pykube.HTTPClient, nodes: List[pykube.Node]) -> Tuple[pykube.Node, pykube.Node]:
+    nginx_po_query = Pod.objects(kube_client).filter(
+        namespace="kube-system",
+        selector={
+            "giantswarm.io/service-type": "managed",
+            "k8s-app": "nginx-ingress-controller"
+        }
+    )
+    nginx_pods = list(nginx_po_query)
+    assert len(nginx_pods) == 1
+    nginx_pod = nginx_pods[0]
+    host_ip = nginx_pod.obj["status"]["hostIP"]
+    other_nodes = [n for n in nodes if host_ip not in [
+        addr["address"] for addr in n.obj["status"]["addresses"]]]
+    return other_nodes[0], other_nodes[1]
+
+
 @pytest.mark.performance
 def test_deployments(kube_client: pykube.HTTPClient, stormforger_load_app_factory: StormforgerLoadAppFactoryFunc,
                      gatling_app_factory: GatlingAppFactoryFunc):
+    # figure out node affinity
+    nodes = list(pykube.Node.objects(kube_client))
+    if len(nodes) >= 3:
+        stormforger_node, gatling_node = get_affinity_nodes(kube_client, nodes)
     stormforger_load_app_factory(8, "loadtest.local")
     gatling_job = gatling_app_factory("https://github.com/giantswarm/nginx-ingress-controller-app/"
                                       + "raw/better-testing/test/kat/NginxSimulation.scala",
