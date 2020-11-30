2f8a09e158a7391e2c647c45393a99836396656b
Jihyuk Bok <jhbok@giantswarm.io>
2020-11-30 10:55:23 +0100

    update integration test

--- integration/test/basic/basic_test.go
+++ integration/test/basic/basic_test.go
@@ -5,11 +5,54 @@ package basic
 import (
 	"context"
 	"testing"
+	"time"
+
+	"github.com/giantswarm/backoff"
+	"github.com/giantswarm/microerror"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestHelm(t *testing.T) {
-	err := ba.Test(context.Background())
+// TestNginx ensures that deployed nginx-ingress-controller is ready
+func TestNginx(t *testing.T) {
+	ctx := context.Background()
+	var err error
+
+	// Check nginx-ingress-controller deployment is ready.
+	err = checkDeploymentReady(ctx)
 	if err != nil {
-		t.Fatalf("%#v", err)
+		t.Fatalf("could not get deployment: %v", err)
 	}
+}
+
+func checkDeploymentReady(ctx context.Context) error {
+	var err error
+
+	l.LogCtx(ctx, "level", "debug", "message", "waiting for deployment ready")
+
+	o := func() error {
+		deploy, err := appTest.K8sClient().AppsV1().Deployments(metav1.NamespaceSystem).Get(ctx, app, metav1.GetOptions{})
+		if apierrors.IsNotFound(err) {
+			return microerror.Maskf(executionFailedError, "deployment %#q in %#q not found", app, metav1.NamespaceSystem)
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+
+		if deploy.Status.ReadyReplicas != *deploy.Spec.Replicas {
+			return microerror.Maskf(executionFailedError, "deployment %#q want %d replicas %d ready", app, *deploy.Spec.Replicas, deploy.Status.ReadyReplicas)
+		}
+
+		return nil
+	}
+	b := backoff.NewConstant(2*time.Minute, 5*time.Second)
+	n := backoff.NewNotifier(l, ctx)
+
+	err = backoff.RetryNotify(o, b, n)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	l.LogCtx(ctx, "level", "debug", "message", "deployment is ready")
+
+	return nil
 }
--- integration/test/basic/error.go
+++ integration/test/basic/error.go
@@ -1,1 +1,9 @@
+// +build k8srequired
+
 package basic
+
+import "github.com/giantswarm/microerror"
+
+var executionFailedError = &microerror.Error{
+	Kind: "executionFailedError",
+}
