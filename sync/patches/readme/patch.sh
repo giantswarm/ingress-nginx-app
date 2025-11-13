c14ded549a6a671b4e5365fa61a21a114edf2f5f
Gerald Pape <gerald@giantswarm.io>
2025-11-13 14:28:34 +0100

    Introduce automated patching of upstream chart
    This introduces a set of scripts and patches that aim to make Giant
    Swarm changes reproducible and discoverable.
    
    Although directory helm/ingress-nginx shouldn't have any changes,
    file helm/ingress-nginx/values.yaml differs only in yaml formatting
    from the version currently present in the main branch.
    
    The included `diffs` directory contains the actual differences
    between the upstream and Giant Swarm chart
    
    Explanation of newly introduced patches:
    
    `chart_yaml`
    
    Aligns the `appVersion` in Chart.yaml from the upstream Chart.yaml
    
    `helpers`
    
    Update common labels helper with `giantswarm.io/service-type: managed`
    and `application.giantswarm.io/team` in _helpers.tpl
    
    `proxy-protocol-configmap`
    
    Add helm template code for PROXY protocol in controller-configmap.yaml
    
    `readme`
    
    Run `helm-docs` to update the README.md of the chart
    
    `services`
    
    Add Giant Swarm specific Service changes
    
    `values`
    
    Patch values.yaml with Giant Swarm specifics

---
+++ sync/patches/readme/patch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+repo_dir=$(git rev-parse --show-toplevel) ; readonly repo_dir
+
+cd "${repo_dir}"
+
+set -x
+
+helm-docs
+
+{ set +x; } 2>/dev/null
