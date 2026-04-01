9005025a6b8174595bcdd1c7b07aef53ff26d386
Matias Charriere <matias@giantswarm.io>
2026-04-01 21:50:11 +0200

    bump bump
    Signed-off-by: Matias Charriere <matias@giantswarm.io>

--- tests/e2e/suites/basic/basic_suite_test.go
+++ tests/e2e/suites/basic/basic_suite_test.go
@@ -10,11 +10,11 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	"github.com/giantswarm/apptest-framework/v3/pkg/state"
-	"github.com/giantswarm/apptest-framework/v3/pkg/suite"
+	"github.com/giantswarm/apptest-framework/v4/pkg/state"
+	"github.com/giantswarm/apptest-framework/v4/pkg/suite"
 
-	"github.com/giantswarm/clustertest/v3/pkg/logger"
-	"github.com/giantswarm/clustertest/v3/pkg/wait"
+	"github.com/giantswarm/clustertest/v4/pkg/logger"
+	"github.com/giantswarm/clustertest/v4/pkg/wait"
 )
 
 const (
